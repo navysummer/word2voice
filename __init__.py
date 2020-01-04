ff42b0eb685f583f31712b2624879fe5590498c0
navysummer <navysummer@yeah.net>
2020-01-04 18:14:24 +0800

    fig bug

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .word2voice import Voice
--- word2voice.py
+++ word2voice.py
@@ -6,11 +6,26 @@ elif major == 3:
     import pyttsx3 as pyttsx
 else:
     raise Exception('current python version is not supported')
-words = "你好"
-engine = pyttsx.init()
-rate = engine.getProperty('rate')
-engine.setProperty('rate', rate-15)
-volume = engine.getProperty('volume')
-engine.setProperty('volume', volume)
-engine.say(words)
-engine.runAndWait()
+
+class Voice(object):
+    """docstring for Voice"""
+    def __init__(self, words=''):
+        super(Voice, self).__init__()
+        self.words = words
+        self.engine = pyttsx.init()
+
+    def start(self,words,rate=200,volume=1.0):
+        self.words = words
+        if self.engine.isBusy():
+            # rate = self.engine.getProperty('rate')
+            # print(rate)
+            self.engine.setProperty('rate', rate-15)
+            # volume = self.engine.getProperty('volume')
+            # print(volume)
+            self.engine.setProperty('volume', volume)
+            self.engine.say(self.words)
+            self.engine.runAndWait()
+
+    def stop(self):
+        if self.engine.isBusy():
+            self.engine.stop()
